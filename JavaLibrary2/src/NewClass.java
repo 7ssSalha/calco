7fece75de18a6b5c074b14749ea943ee6ecb4bfb
7ssSalha <husseinsoheil1999@hotmail.com>
2020-02-07 21:36:58 +0200

    4th try

---
+++ JavaLibrary2/src/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hussein
+ */
+public class NewClass {
+    	public static void print(String a) {
+		System.out.print(a);;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print("hussein");
+                print("hussein");
+	}
+
+}
